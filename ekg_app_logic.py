3856b9edf70bb7baedc2ed9e467b7b781a9539c9
Arkadiusz Karbowski <126800571+ArkadiuszKarbowski@users.noreply.github.com>
2023-05-17 15:12:22 +0200

    gui fixed

--- ekg_app_logic.py
+++ ekg_app_logic.py
@@ -23,12 +23,13 @@ class EkgApp(QtWidgets.QMainWindow):
         self.threadpool = QtCore.QThreadPool()
 
         self.canvas = EKGplot(self, width=4, height=3, dpi=100)
+        self.canvas.setStyleSheet("background-color: transparent;")
         self.ui.gridLayout.addWidget(self.canvas, 3, 1, 1, 3)
         self.reference_plot = None
         self.q = queue.Queue(maxsize=20)
 
         self.plotdata = np.zeros((1,200))
-        self.ekg_reader = EkgReader(self)
+       # self.ekg_reader = EkgReader(self)
 
         self.interval = 50
         self.x_vals = []
@@ -40,7 +41,7 @@ class EkgApp(QtWidgets.QMainWindow):
         self.timer.setInterval(self.interval) #msec
         self.timer.timeout.connect(self.updatePlot)
         self.timer.start()
-        self.start_workers()
+     #   self.start_workers()
     
 
 
@@ -55,54 +56,20 @@ class EkgApp(QtWidgets.QMainWindow):
         self.canvas.axes.legend(loc='upper left')
         self.canvas.axes.set_xlabel('Sample')
         self.canvas.axes.set_ylabel('Data')
+        self.canvas.axes.set_facecolor((1, 1, 1, 0.1))
 
         self.canvas.draw()
         self.canvas.show()
 
-
-
-
-
-    # def update_plot(self):
-    #     try:
-    #         data=[0]
-    #         while True:
-    #             try:
-
-    #                 data = self.q.get_nowait()
-    #             except queue.Empty:
-    #                 break
-                        
-
-
-
-    #             shift = len(data)
-    #             self.plotdata = np.roll(self.plotdata, -shift,axis = 0)
-    #             self.plotdata[-shift:,:] = data
-    #             self.ydata = self.plotdata[:]
-    #             self.canvas.axes.set_facecolor((0,0,0))
-                
-        
-    #             if self.reference_plot is None:
-    #                 plot_refs = self.canvas.axes.plot( self.ydata, color=(0,1,0.29))
-    #                 self.reference_plot = plot_refs[0]				
-    #             else:
-    #                 self.reference_plot.set_ydata(self.ydata)
-
-                
-    #     except:
-    #         pass
     
-    def start_workers(self):
-        saver = Worker(self.ekg_reader.save_to_csv, 'data.csv')
-        reader = Worker(self.ekg_reader.read)
+    # def start_workers(self):
+    #     saver = Worker(self.ekg_reader.save_to_csv, 'data.csv')
+    #     reader = Worker(self.ekg_reader.read)
 
-        self.threadpool.start(reader)
-        self.threadpool.start(saver)
+    #     self.threadpool.start(reader)
+    #     self.threadpool.start(saver)
     
     
-
-
 
 if __name__ == "__main__":
     import sys
--- models/ekg_plot.py
+++ models/ekg_plot.py
@@ -8,6 +8,8 @@ class EKGplot(FigureCanvas):
 	def __init__(self, parent=None, width=5, height=4, dpi=100):
 		fig = Figure(figsize=(width, height), dpi=dpi)
 		self.axes = fig.add_subplot(111)
+		fig.patch.set_alpha(.5)
+		
 		super(EKGplot, self).__init__(fig)
 		fig.tight_layout()
 		
